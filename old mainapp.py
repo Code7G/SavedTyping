49e671a701c0f40f2c1f9a12122a096aa421e619
Code7G <111247923+Code7G@users.noreply.github.com>
2022-10-16 19:48:28 +0200

    big update

--- old mainapp.py
+++ old mainapp.py
@@ -1,5 +1,9 @@
 from pathlib import Path  # for creating save folders and files
 
+# This app is an older version of the newest app (mainapp.py)
+# This map creates multiple files for every save while the newer one creates only one with the same amount of data
+# Use this app if you prefer to however it will take unnececry procesing
+
 
 # the main function for user input
 def writing():
@@ -17,9 +21,40 @@ def writing():
 
 
 save_file = []  # short term save file
-all_saves = Path('Saves\All_saves.txt')  # location of the save file
+path = Path('Saves')  # location of the save files
+all_saves = Path('Saves\All_saves1~.txt')
+
+# identifying the save files for creating new ones:
+all_path = path.rglob('*.txt')
+last_save_file = []
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+files = {}
+
+for i in all_path:
+    last_save_file.append(str(i))
+for c in last_save_file:
+    if '~' in c:  # the '~' symbol is used to identify the files generated from the program
+        for l in c:
+            if l in numbers:
+                # getting the number value of the files
+                files[c] = int(files.get(c, 0)) + int(l)
+
 
 if all_saves.exists():
+    all_numbers = []
+
+    for item in files:
+        all_numbers.append(files.get(item))
+
+    # finding the largest number in the files
+    max = 0
+    for num in all_numbers:
+        if max < num:
+            max = num
+
+    # creating the name for the new file
+    all_saves_second = Path(f'Saves/All_saves{max + 1}~.txt')
+
     while True:
         writing_e = writing()
         save_file.append(writing_e)
@@ -27,13 +62,11 @@ if all_saves.exists():
         if 'help' in save_file:
             save_file.remove('help')
 
-        # saving the changes to the save file
         if writing_e == 'end':
             save_file.remove('end')
-            all_saves.write_text(f'''
-            {all_saves.read_text()}
-                                 
-            {str(save_file)}''')
+            # using the new name for file creation
+            all_saves_second.write_text(str(save_file))
+            print(f'Full save >>>>{all_saves_second.read_text()}<<<<')
             break
 
         if writing_e == 'saved':
@@ -51,10 +84,10 @@ if all_saves.exists() == False:
         if writing_e == 'end':
             save_file.remove('end')
 
-            # creating the 'Saves' folder and the save file
+            # creating the 'Saves' folder and the first save file
             folder = Path("Saves/")
             folder.mkdir(parents=True, exist_ok=True)
-            fn = "All_saves.txt"
+            fn = "All_saves1~.txt"
             filepath = folder / fn
             with filepath.open("w", encoding="utf-8") as file:
                 file.write(str(save_file))
